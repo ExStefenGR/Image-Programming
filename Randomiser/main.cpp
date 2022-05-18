989446df1c6d45372b6249c8d4712638bb90e1c5
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2022-05-18 09:44:47 +0100

    Minor fix

--- Randomiser/main.cpp
+++ Randomiser/main.cpp
@@ -3,23 +3,25 @@
 #include <vector>
 #include <cmath>
 #include <fstream>
-
 //My first attempt at image programming 15/01/2022
-
-const int width = 512, height = 512;
+int width = 0, height = 0;
+int R, G, B, W;
 
 int main()
 {
+	std::cin >> width;
+	std::cin >> height;
+	std::cin >> W;
+	std::cin >> B;
 	srand(time(NULL));
-	std::ofstream img ("picture.ppm");
-	img << "P3" << std::endl;
+	std::ofstream img ("picture.pgm");
+	img << "P2" << std::endl;
 	img << width << " " << height << std::endl;
 	img << "255" << std::endl;
-
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
 			
-			img << rand() % 255 << " " << rand() % 255 << " " << rand() % 255 << std::endl;
+			img << rand() % W << " " << rand() % B;
 		}
 	}
 		return 0;
